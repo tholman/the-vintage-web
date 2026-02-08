aff9f9ac9af9101354f2039c59de827391380330
Tim Holman <timothy.w.holman@gmail.com>
2026-02-08 14:02:49 -0500

    initial commit

---
+++ sites.js
@@ -0,0 +1,10 @@
+var sites = [
+  'https://www.fieggen.com/shoelace/index.htm',
+  'https://www.spacejam.com/1996/',
+  "https://www.pmichaud.com/toast/",
+  "https://www.fogcam.org/",
+  "https://robspuzzlepage.com/jumping.htm",
+  "http://www.spelljammer.org/",
+  "https://milliondollarhomepage.com/",
+  
+];
